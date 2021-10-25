d2fd1145d95c156556211ea624f320e94cdbcd35
Folkert Bleichrodt <fbleichrodt@gmail.com>
2021-10-25 11:38:03 +0200

    Applied statuscmd patch for clickable links

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,18 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    {"", "sb-news",     60,     1},
+    {"", "sb-rain",     300,    2},
+    {"", "sb-moon",     1800,   3},
+    {"", "sb-cpu",      30,     4},
+    {"", "sb-memory",   30,     5},
+    {"", "sb-clock",    5,      6},
+    {"", "sb-volume",   0,      7},
+    {"", "sb-internet", 30,     8},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+//static char delim[] = " | ";
+//static unsigned int delimLen = 5;
+static char delim[] = " ";
 static unsigned int delimLen = 5;
